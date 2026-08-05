1c9fcbdafe1a121c7aa7a9d6862fd399b26968a0
Richard Cane <29371470+richardcane@users.noreply.github.com>
2026-08-05 23:27:00 +0100

    Add data field to eligibility result entity

--- info-and-advice-datastore-service/src/main/java/uk/gov/justice/laa/ia/datastore/entity/EligibilityResultEntity.java
+++ info-and-advice-datastore-service/src/main/java/uk/gov/justice/laa/ia/datastore/entity/EligibilityResultEntity.java
@@ -1,21 +1,24 @@
 package uk.gov.justice.laa.ia.datastore.entity;
 
+import java.time.Instant;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
 import com.fasterxml.jackson.databind.JsonNode;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.time.Instant;
-import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
 
 /** Entity to represent eligibility results. */
 @Data
@@ -33,6 +36,10 @@ public class EligibilityResultEntity {
 
   @Column(name = "application_id", nullable = false)
   private UUID applicationId;
+
+  @JdbcTypeCode(SqlTypes.JSON)
+  @Column(name = "data")
+  private JsonNode data;
 
   @JdbcTypeCode(SqlTypes.JSON)
   @Column(name = "result_json", nullable = false)
